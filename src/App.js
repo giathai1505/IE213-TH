96464488469b119738590725ebece88a33690dc3
Ngô Gia Thái <81607294+Thai-Sieu-Cap-VIP-Pro@users.noreply.github.com>
2022-05-16 11:48:51 +0700

    Thai

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Movie from "./components/movie";
+import MoviesList from "./components/movies-list";
+import Login from "./components/login";
+import AddReview from "./components/add-review";
+
+import { Nav, Navbar } from "react-bootstrap";
+import { Link, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+  async function login(user = null) {
+    setUser(user);
+  }
+  async function logout() {
+    setUser(null);
+  }
+
+  //19522187 - Ngô Gia Thái
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes>
+        <Route index element={<MoviesList />}></Route>
+        <Route path="/movies" element={<MoviesList />}></Route>
+        <Route
+          path="/movies/:id/review"
+          element={<AddReview user={user} />}
+        ></Route>
+        <Route path="/movies/:id" element={<Movie user={user} />}></Route>
+        <Route path="/login" element={<Login login={login} />}></Route>
+      </Routes>
     </div>
   );
 }
